bc25a7567c5385ba6c922087f2131d3438b93a79
Marius Andra <marius.andra@gmail.com>
2019-06-03 09:04:01 +0200

    [wip] kill forks on reset

--- src/__tests__/saga-context.js
+++ src/__tests__/saga-context.js
@@ -7,8 +7,10 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import { mount, configure } from 'enzyme'
 import { Provider } from 'react-redux'
-import { put } from 'redux-saga/effects'
+import { put, delay } from 'redux-saga/effects'
 import Adapter from 'enzyme-adapter-react-16'
+
+const promiseDelay =  ms => new Promise((resolve) => setTimeout(resolve, ms))
 
 configure({ adapter: new Adapter() })
 
@@ -70,4 +72,67 @@ test('sagas stop when context resets', () => {
   expect(counter).toBe(2)
 
   unmount2()
+})
+
+test('forks stop when context resets', async () => { 
+  let counter = 0
+
+  resetContext({ plugins: [ sagaPlugin ] })
+  getStore()
+
+  const logic = kea({
+    actions: () => ({
+      increment: true
+    }),
+
+    reducers: ({ actions }) => ({
+      reducerCounter: [0, {
+        [actions.increment]: (state) => state + 1
+      }],
+    }),
+
+    takeEvery: ({ actions }) => ({
+      [actions.increment]: function * () {
+        yield delay(1000)
+        counter += 1
+      }  
+    })
+  })
+
+  expect(counter).toBe(0)
+  logic.mount()
+  expect(counter).toBe(0)
+
+  const { store } = getContext()
+  store.dispatch(logic.actions.increment())
+
+  // reducerCounter increments immediately
+  expect(logic.selectors.reducerCounter(store.getState())).toBe(1)
+
+  // counter increments after a delay
+  expect(counter).toBe(0)
+  await promiseDelay(1001)
+  expect(counter).toBe(1)
+
+  // dispatch new action 
+  store.dispatch(logic.actions.increment())
+
+  // and reset the context
+  resetContext({ plugins: [ sagaPlugin ] })
+  getStore()
+
+  const { store: store2 } = getContext()
+
+  logic.mount()
+
+  expect(logic.selectors.reducerCounter(store2.getState())).toBe(0)
+  expect(counter).toBe(1)
+
+  store2.dispatch(logic.actions.increment())
+
+  expect(logic.selectors.reducerCounter(store2.getState())).toBe(1)
+
+  expect(counter).toBe(1)
+  await promiseDelay(1001)
+  // expect(counter).toBe(2) // should not be 3!
 })

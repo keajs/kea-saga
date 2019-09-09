90433d3adb9232273fd189403927013c493f93a2
Marius Andra <marius.andra@gmail.com>
2019-09-09 12:25:03 +0200

    rc.8 compatibility

--- src/saga/index.js
+++ src/saga/index.js
@@ -16,10 +16,14 @@ export default {
     saga: undefined
   }),
 
+  buildOrder: {
+    connectSagas: { after: 'connect' }
+  },
+
   buildSteps: {
     // 1) Add to .connectedSagas any generator functions in input.sagas || input.connect.sagas
     // 2) Connect logic stores from input.sagas || input.connect.sagas into .connections
-    connect (logic, input) {
+    connectSagas (logic, input) {
       let connectedSagas = []
 
       if (input.sagas || (input.connect && input.connect.sagas)) {
